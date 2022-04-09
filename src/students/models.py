5e27aa41a147fff5e0fffaba6b8edc59b2cd7870
aquadeem <aoire15@gmail.com>
2022-04-09 19:23:58 +0300

    added person model

--- src/students/models.py
+++ src/students/models.py
@@ -7,3 +7,14 @@ class Student(Model):
 
     first_name = CharField(max_length=50)
     last_name = CharField(max_length=50)
+
+
+class Person(Model):
+
+    first_name = CharField(max_length=50)
+    last_name = CharField(max_length=50)
+    age_person = CharField(max_length=50)
+    type_person = CharField(max_length=50)
+    create_person = CharField(max_length=50)
+    update_person = CharField(max_length=50)
+    status_person = CharField(max_length=50)
